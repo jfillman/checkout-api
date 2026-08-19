e6a6cccf4661dd95c74d8a62afcf4f313358f210
jfillman <crossfader@gmail.com>
2026-08-18 21:19:21 -0700

    Add config validation call on startup (AI-triage source-bug test - will revert)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const port = process.env.PORT || 3000;
+validateCheckoutConfig();
 
 const server = http.createServer((req, res) => {
   if (req.url === '/healthz') {
