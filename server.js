1087482ac59f78da60edbc0b01e4c28920c5bf4b
jfillman <crossfader@gmail.com>
2026-08-24 09:42:06 -0700

    updates

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'checkout-api: greetings!!!',
+    message: 'checkout-api: welcome!!!',
     version: process.env.APP_VERSION || 'dev',
   });
 });
