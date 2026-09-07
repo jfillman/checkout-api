ad0460df501b82cc440e883b15d3c15e615e58ce
James Fillman <jamesfillman@gmail.com>
2026-09-06 20:34:33 -0700

    NodeJSApplication scaffold: index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const server = http.createServer((req, res) => {
     return;
   }
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end("checkout-api: Hello!!\\n");
+  res.end("Hello from checkout-api\\n");
 });
 
 server.listen(port, () => {
-  console.log(`checkout-api listening on port ${port}`);
+  console.log("checkout-api listening on port ${port}");
 });
