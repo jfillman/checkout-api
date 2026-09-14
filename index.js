730a3e732b501665bb0d0edeaed60bd017c85c59
James Fillman <jamesfillman@gmail.com>
2026-09-13 22:10:36 -0700

    Change greeting message to 'Hello World'

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
     return;
   }
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end("checkout-api: Hello!!\\n");
+  res.end("checkout-api: Hello World!!\\n");
 });
 
 server.listen(port, () => {
